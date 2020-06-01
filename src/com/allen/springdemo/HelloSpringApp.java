b05449711c0b5f7000364fda1fb97ddfce909f67
Yunfei Zhang <beethovenzhang@gmail.com>
2020-05-31 21:22:14 -0700

    First spring app

---
+++ src/com/allen/springdemo/HelloSpringApp.java
@@ -0,0 +1,16 @@
+package com.allen.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+        // Load the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        // Call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        // close the context
+        context.close();
+    }
+}
