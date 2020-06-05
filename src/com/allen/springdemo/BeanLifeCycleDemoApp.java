e5d7579d8bc07ddc4525b011d2926913a41f3622
Yunfei Zhang <beethovenzhang@gmail.com>
2020-06-05 16:40:52 -0700

    Bean lifecycle

---
+++ src/com/allen/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,16 @@
+package com.allen.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+    public static void main(String[] args) {
+        // load Spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        context.close();
+
+    }
+}
--- src/com/allen/springdemo/TrackCoach.java
+++ src/com/allen/springdemo/TrackCoach.java
@@ -21,4 +21,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it! " + this.fortuneService.getFortune();
     }
+
+    // Add an init method
+    public void doMyStartUpStuff() {
+        System.out.println("TrackCoach: inside method doMyStartUpStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanUpStuff() {
+        System.out.println("TrackCoach: inside method doMyCleanUpStuff");
+    }
 }
